938715bcc9d2f88c0d4244e8e14a2164bee1eb67
John Varga <john.g.varga@gmail.com>
2016-02-09 07:42:18 -0800

    last date

--- src/components/jobStatus/jobStatus.js
+++ src/components/jobStatus/jobStatus.js
@@ -16,11 +16,12 @@ angular.module('unifi-jenkins')
 			function refresh() {
 				jobsFactory.getJob(scope.name).then(function(r) {
 					scope.item = r.data;
+					scope.lastUpdate = new Date();
 					$log.log('-job status- got job data', r.data);
 				});
 			}
 
-			$interval(refresh, 1000 * 10);
+			$interval(refresh, 1000 * 30);
 
 			refresh();
 		}

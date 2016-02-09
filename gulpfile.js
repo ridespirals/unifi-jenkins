29e07e2c1883967696259e0cd5973e3d7b5a22fe
John Varga <john.varga@inviewlabs.com>
2016-02-08 16:39:10 -0800

    more gulp tasks; can watch changes and re-run electron

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,22 @@ var sequence 	= require('run-sequence');
 var merge		= require('merge-stream');
 var runElectron = require('gulp-run-electron');
 
+gulp.task('app', function() {
+	gulp.src('src')
+		.pipe(runElectron([], { cwd: 'src' }));
+});
+
 
 gulp.task('sass', function() {
 	return gulp.src(['./src/sass/*.scss'])
 		.pipe(gulpSass())
 		.pipe(gulp.dest('./src'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('src/sass/*.scss', ['sass', runElectron.rerun]);
+});
+
+gulp.task('default', function() {
+	sequence(['sass'], ['watch', 'app']);
 });
